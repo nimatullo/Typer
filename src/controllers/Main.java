ab031a702fb013b34eb6a0d5abd833e1ffba724b
Sherzod Nimatullo <shermanjones99@gmail.com>
2019-12-15 21:59:15 -0500

    post presentation

--- src/controllers/Main.java
+++ src/controllers/Main.java
@@ -3,8 +3,10 @@ package controllers;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.ImageCursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -12,10 +14,10 @@ import javafx.stage.StageStyle;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("../view/sample.fxml"));
         primaryStage.setTitle("Typer");
-        Scene scene = new Scene(root, 1280, 720);
+        Scene scene = new Scene(root,1280,720);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
